2e1c34bbe61ce74dd152677f86a025caf4d7a15c
Konrad Sawras <zawraz@gmail.com>
2022-12-16 13:38:29 +0100

    Final commit
    A mini-full stack blog site w/node.js

--- app.js
+++ app.js
@@ -1,30 +1,75 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
+
 const app = express();
 
-// listen for req
-app.listen(3000);
+// connect to MongoDB
+const dbURI =
+	"mongodb+srv://zawraz:test1234@node-savvy-blog.qcknjpg.mongodb.net/node?retryWrites=true&w=majority";
+mongoose.set("strictQuery", false);
+mongoose
+	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => {
+		console.log("connected to MongoDB");
+		app.listen(3000);
+	})
+	.catch((err) => console.log(err));
+
+// register view engine
+app.set("view engine", "ejs");
+
+// app.set('views', 'myViews')
+
+//middleware & static files
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+// app.get("/add-blog", (req, res) => {
+// 	const blog = new Blog({
+// 		title: "Second Blog",
+// 		snippet: "About my second blog",
+// 		body: "Content of my second blog",
+// 	});
+
+// 	blog.save()
+// 		.then((result) => {
+// 			res.send(result);
+// 		})
+// 		.catch((err) => console.log(err));
+// });
+
+// app.get("/all-blogs", (req, res) => {
+// 	Blog.find()
+// 		.then((result) => res.send(result))
+// 		.catch((err) => console.log(err));
+// });
+
+// app.get("/single-blog", (req, res) => {
+// 	Blog.findById()
+// 		.then((result) => res.send(result))
+// 		.catch((err) => console.log(err));
+// });
 
 app.get("/", (req, res) => {
 	// res.write("test");
 	// res.end();
 
 	// res.send("<p>home page</p>");
-
-	res.sendFile("./views/index.html", { root: __dirname });
+	res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
-	// res.send("<p>about page</p>");
-
-	res.sendFile("./views/about.html", { root: __dirname });
+	res.render("about", { title: "About" });
 });
 
-// redirecting
-app.get("/about-us", (req, res) => {
-	res.redirect("/about");
-});
+// blog routes
+app.use("/blogs", blogRoutes);
 
 // 404 case
 app.use((req, res) => {
-	res.status(404).sendFile("./views/404.html", { root: __dirname });
+	res.status(404).render("404", { title: "404" });
 });
